c38e4fc53c9e2330f98a82db496695de7ec3b703
phohead <wilsonyanglol@gmail.com>
2023-10-03 18:14:51 -0400

    Uploading Resume

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,3 +1,10 @@
+/*
+File Name: COMP229_Lab1
+Student Name: Wilson Yang
+Student Numb: 301195179
+Date: October 03, 2023
+*/
+
 exports.render = function(req, res) { 
 	res.render('index', {
         title: 'Home Page'
--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,3 +1,10 @@
+/*
+File Name: COMP229_Lab1
+Student Name: Wilson Yang
+Student Numb: 301195179
+Date: October 03, 2023
+*/
+
 var express = require('express');
 var router = express.Router();
 
